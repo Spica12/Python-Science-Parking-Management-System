3be51486c509ffe32bceb57612f1df0f9b7fead0
Vitalii Savenko <vitaly.savenko12@gmail.com>
2024-07-22 15:10:00 +0300

    add Vehicle_list_new_delete

--- parking_service/models.py
+++ parking_service/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+
+class LicensePlate(models.Model):
+    plate_number = models.CharField(max_length=10)
+    accuracy = models.FloatField()
+    image = models.ImageField(upload_to='license_plates/')
+    detected_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.plate_number
+
+
+class Vehicle(models.Model):
+    plate_number = models.CharField(max_length=10)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
+
+    created_at = models.DateTimeField(auto_now_add=True)
--- parking_service/views.py
+++ parking_service/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from parking_service.models import Vehicle
+from parking_service.forms import VehicleForm
 
 # Create your views here.
 def main_page(request):
@@ -7,3 +10,34 @@ def main_page(request):
         request,
         "parking_service/index.html",
     )
+
+@login_required(login_url="login")
+def get_vehicles(request):
+    vehicles = Vehicle.objects.filter(user=request.user)
+
+    return render(
+        request,
+        "parking_service/vehicles.html",
+        context={'vehicles': vehicles}
+    )
+
+@login_required(login_url="login")
+def add_vehicle(request):
+    if request.method == "POST":
+        form = VehicleForm(request.POST)
+        if form.is_valid():
+            vehicle = form.save(commit=False)
+            vehicle.save()
+            return redirect("parking_service:vehicles")
+    else:
+        form = VehicleForm()
+
+    return render(request, "parking_service/new_vehicle.html", {"form": form})
+
+@login_required(login_url="login")
+def del_vehicle(request, pk):
+    vehicle = get_object_or_404(Vehicle, pk=pk)
+    if vehicle is not None:
+        vehicle.delete()
+
+    return redirect("parking_service:vehicles")
