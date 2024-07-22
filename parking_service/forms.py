5bf54448de7e1024c1a4332e33b8ddfd53a93377
Vitalii Savenko <vitaly.savenko12@gmail.com>
2024-07-22 23:28:37 +0300

    add form to index

--- parking_service/forms.py
+++ parking_service/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
-from parking_service.models import Vehicle
+from parking_service.models import Vehicle, LicensePlate
+
+class LicensePlateForm(forms.ModelForm):
+    plate_number = forms.CharField(
+        min_length=3, max_length=10, required=True, widget=forms.TextInput()
+    )
+    image = forms.ImageField()
+
+    class Meta:
+        model = LicensePlate
+        fields = ("image", 'plate_number')
 
 
 class VehicleForm(forms.ModelForm):
